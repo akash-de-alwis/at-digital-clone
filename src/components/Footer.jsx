db3289b54fc49d3d9e23f52709c5f578e10cdefd
Akash De Alwis <akashdealwis@gmail.com>
2025-06-25 12:06:39 +0530

    footer logo updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import logo from '../assets/logo.png';
+import logo from '../assets/Logo.png';
 
 function Footer() {
   return (
